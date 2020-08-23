dd00ff8b03800894a9ea991b4319ce531a228714
Pouya Moazzezi <pmoazzezi@gmail.com>
2020-08-23 04:44:43 +0430

    Removed adding a lot of extra lines, and right-end white space  when getting input.

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import io
 import os
 
+
 print("Web Automation v0.5.0 ")
 print("Created by Pouya Moazzezi")
 print("==========================================================")
 
-project_path = input("\nWhere do you want to create the projects? [Default: Here]\t")
+project_path = input("Where do you want to create the projects? [Default: Here]: ")
 parent_directory = os.path.dirname(project_path)
 
 if project_path.strip() == "":
@@ -16,85 +17,85 @@ while not (os.path.isdir(project_path) or project_path == ""): #if it doesn't Ex
     project_path = ""
   if not(os.path.isdir(parent_directory)):
     print("Please enter a valid directory")
-    project_path = input("\nWhere do you want to create the projects? [Default: Here]\t")
+    project_path = input("Where do you want to create the projects? [Default: Here]: ")
   else: #if Parent Directory Exists
-    make_folder_confirmation = input("Do you want to make the folder: " + project_path + " y/n [y]\t")
+    make_folder_confirmation = input(f"Do you want to make the folder: \"{project_path}\" y/n [y]: ")
     if make_folder_confirmation.strip() == "":
       make_folder_confirmation = "y"
     while not (make_folder_confirmation == "y" or make_folder_confirmation == "n"):
       print("Didn't catch that.")
-      make_folder_confirmation = input("Do you want to make the folder: " + project_path + " y/n [y]\t")
+      make_folder_confirmation = input(f"Do you want to make the folder: \"{project_path}\" y/n [y]: ")
 
     os.mkdir(project_path)
 
 
 #==================================================== HTML ====================================================
-html_name = input("\nType a name for your html file name [index]\t")
+html_name = input("Type a name for your html file name [index]: ")
 if html_name.strip() == "":
   html_name = "index"
 
-html_extention = input("\nYour preferable html file extention (htm | html) [html]\t")
+html_extention = input("Your preferable html file extention (htm | html) [html]: ")
 if html_extention.strip() == "":
   html_extention = "html"
 
 while (html_extention != "htm" and html_extention != "html"):
   print("Didn't catch that.")
-  html_extention = input("\nYour preferable html file extention (htm | html) [html]\t")
+  html_extention = input("Your preferable html file extention (htm | html) [html]: ")
   if html_extention.strip() == "":
     html_extention = "html"
 
-html_title_name = input("\nType a title for your page [Document]\t")
+html_title_name = input("Type a title for your page [Document]: ")
 if html_title_name.strip() == "":
   html_title_name = "Document"
 
 
 #==================================================== Styles ====================================================
 
-css_file_name = input("\nType a name for your stylesheet file [style]\t")
+css_file_name = input("Type a name for your stylesheet file [style]: ")
 if css_file_name.strip() == "":
   css_file_name = "style"
 
-put_css_in_folder = input("\nYou want to put ccs files in folder? y/n [n]\t")
+put_css_in_folder = input("You want to put ccs files in folder? y/n [n]: ")
 if put_css_in_folder.strip() == "":
   put_css_in_folder = "n"
 
 while not(put_css_in_folder == "y" or put_css_in_folder == "n"):
   print("Didn't catch that.")
-  put_css_in_folder = input("\nYou want to put stylesheet file in a folder? y/n [n]\t")
+  put_css_in_folder = input("You want to put stylesheet file in a folder? y/n [n]: ")
   if put_css_in_folder.strip() == "":
     put_css_in_folder = "n"
 
 
 style_folder_name = ""
 if put_css_in_folder == "y":
-  style_folder_name = input("\nType the name for your styles folder [css]\t")
+  style_folder_name = input("Type the name for your styles folder [css]: ")
   if style_folder_name.strip() == "" :
     style_folder_name = "css"
 
 
-css_extention = input("\nType the name of stylesheet file extention (css | less | sass | scss) [css]\t")
+css_extention = input("Type the name of stylesheet file extention (css | less | sass | scss) [css]: ")
 if css_extention.strip() == "":
   css_extention = "css"
 
 while not(css_extention == "css" or css_extention == "scss" or css_extention == "sass" or css_extention == "less"):
-  css_extention = input("\nType the name of stylesheet file extention (css | less | sass | scss) [css]\t")
+  css_extention = input("Type the name of stylesheet file extention (css | less | sass | scss) [css]: ")
   if css_extention.strip() == "":
     css_extention = "css"
 
 
 #==================================================== Styles ====================================================
 
-js_name = input("\nType a name for your script file name [script]\t")
+js_name = input("Type a name for your script file name [script]: ")
 if js_name.strip() == "":
   js_name = "script"
 
-put_js_in_folder = input("\nYou want to put js file in a folder? y/n [n]\t")
+put_js_in_folder = input("You want to put js file in a folder? y/n [n]: ")
 if put_js_in_folder.strip() == "":
   put_js_in_folder = "n"
 
 js_folder_name = "" 
 if put_js_in_folder.strip() == "y":
-  js_folder_name = input("\nType the name for your JavaScript folder [js]\t")
+  js_folder_name = input("Type the name for your JavaScript folder [js]: ")
   if js_folder_name.strip() == "" :
     js_folder_name = "js"
 
@@ -129,7 +130,7 @@ with open(os.path.join(project_path, js_path), "w") as js:
 html_file = html_name + ("." + html_extention)
 html_path = os.path.join(html_file)
 with open(os.path.join(project_path, html_path), "w") as html:
-    html.write('''<!DOCTYPE html>
+    html.write(f'''<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
@@ -141,9 +142,7 @@ with open(os.path.join(project_path, html_path), "w") as html:
     <script src="{js_path}"></script>
   </body>
 </html>
-'''.replace("{html_title_name}", html_title_name)
-  .replace("{stylesheet_path}", stylesheet_path)
-  .replace("{js_path}", js_path)) # Replaces the title with given name, so as js and stylesheet file path
+''') # Replaces the title with given name, so as js and stylesheet file path
 
 input("\nFiles were successfully created!\nPress ENTER to close the program")
 
